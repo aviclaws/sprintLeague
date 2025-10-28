80f04e5a54394338d58fe0895d21828f33daac98
Avi Klausner <avraham.klausner@ll.mit.edu>
2025-10-27 23:01:47 -0400

    Fixed db errors

--- app.py
+++ app.py
@@ -9,15 +9,6 @@ from constants import *
 
 st.set_page_config(page_title="Stopwatch with Login & DB", page_icon="⏱️", layout="centered")
 
-from pathlib import Path
-
-DB_PATH = Path(__file__).with_name("stopwatch.db")
-
-if DB_PATH.exists():
-    st.write()
-    DB_PATH.unlink()  # delete the old file
-
-
 init_db()
 
 # --- SESSION STATE ---
